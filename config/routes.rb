c9d602da98f3c14cbcb1d7d74947488c00b43279
Michelle Brien <michelle.brien12@gmail.com>
2017-12-15 12:34:39 -0300

    setup main routes and static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+
+  get "/why-open-source", to: "pages#why"
+  get "/first-pull-request", to: "pages#first"
+  get "/resources", to: "pages#resources"
+
 end
