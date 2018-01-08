e487f2f0047f923472d656e98e3ac4d7db1dfba7
Michelle Brien <michelle.brien12@gmail.com>
2018-01-08 18:39:09 -0500

    basic data parsed and console logged

--- frontend/components/issues/issues.js
+++ frontend/components/issues/issues.js
@@ -1,1 +1,11 @@
 import "./issues.css";
+
+// const ul = document.getElementById("issues");
+const url =
+  "https://api.github.com/search/issues?q=label:beginner+language:ruby+state:open&sort=created";
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  });
