522d6d5a1bd657618ef622296b574f7fb90dd952
Michelle Brien <michelle.brien12@gmail.com>
2018-01-08 19:28:17 -0500

    printing one title

--- frontend/components/issues/issues.js
+++ frontend/components/issues/issues.js
@@ -1,11 +1,12 @@
 import "./issues.css";
 
-// const ul = document.getElementById("issues");
+const issueList = document.getElementById("issues");
 const url =
-  "https://api.github.com/search/issues?q=label:beginner+language:ruby+state:open&sort=created";
+  "https://api.github.com/search/issues?q=label:beginner+language:html+state:open&sort=created";
 
 fetch(url)
   .then(response => response.json())
   .then(data => {
-    console.log(data);
+    const issueTitle = data.items[0].title;
+    issueList.innerHTML = `<li>${issueTitle}</li>`;
   });
