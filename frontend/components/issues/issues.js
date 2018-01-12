d95da12ed0d648109c5a17d2956adacf0bf85794
Michelle Brien <michelle.brien12@gmail.com>
2018-01-12 16:42:42 +0000

    added comments and cleaned up html

--- frontend/components/issues/issues.js
+++ frontend/components/issues/issues.js
@@ -1,6 +1,6 @@
 import "./issues.css";
 
-const issueList = document.getElementById("issues");
+const issueContainer = document.getElementById("issues");
 const url =
   "https://api.github.com/search/issues?q=label:beginner+language:html+state:open&sort=created";
 
@@ -8,9 +8,9 @@ fetch(url)
   .then(response => response.json())
   .then(data => {
     data.items.forEach(issue => {
-      const issueTitle = issue.title;
-      const dateOpened = issue.created_at;
-      issueList.innerHTML += `<li>Title = ${issueTitle}</li>`;
-      issueList.innerHTML += `Opened on ${dateOpened}`;
+      issueContainer.innerHTML += `<h3>${issue.title}</h3>
+                                   <p>Github repo</p>
+                                   <p>${issue.created_at}
+                                   <p>${issue.comments}</p>`;
     });
   });
