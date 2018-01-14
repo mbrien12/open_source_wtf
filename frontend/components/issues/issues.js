17a7a5400cf2b18a28004ffefcccd960a1b23459
Michelle Brien <michelle.brien12@gmail.com>
2018-01-14 15:12:20 +0000

    displays repo name

--- frontend/components/issues/issues.js
+++ frontend/components/issues/issues.js
@@ -3,9 +3,6 @@ import "./issues.css";
 const issueContainer = document.getElementById("issues");
 const GitUrlParse = require("git-url-parse"); // https://www.npmjs.com/package/git-url-parse
 
-console.log(
-  GitUrlParse("git@github.com:IonicaBizau/node-git-url-parse.git").name
-);
 const url =
   "https://api.github.com/search/issues?q=label:beginner+no:assignee+language:html+state:open&sort=created";
 
@@ -13,10 +10,9 @@ fetch(url)
   .then(response => response.json())
   .then(data => {
     data.items.forEach(issue => {
-      const repoName = GitUrlParse(`${issue.html_url}`).name;
-      console.log(repoName);
+      const repoName = GitUrlParse(`${issue.html_url}`).name.split("/issues");
       issueContainer.innerHTML += `<h3>${issue.title}</h3>
-                                   <p>${repoName}</p>
+                                   <p>${repoName[0]}</p>
                                    <p>${issue.created_at}
                                    <p>${issue.comments}</p>`;
     });
