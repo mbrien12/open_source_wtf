4f6e002bd9f85680f7e434382edd3b6a143bf71a
Michelle Brien <michelle.brien12@gmail.com>
2018-01-14 16:13:22 +0000

    parsed date

--- frontend/components/issues/issues.js
+++ frontend/components/issues/issues.js
@@ -11,11 +11,11 @@ fetch(url)
   .then(data => {
     data.items.forEach(issue => {
       const repoName = GitUrlParse(`${issue.html_url}`).name.split("/issues");
+      const dateSplit = `"${issue.created_at}"`.split("T");
+
       issueContainer.innerHTML += `<h3>${issue.title}</h3>
                                    <p>${repoName[0]}</p>
-                                   <p>${issue.created_at}
+                                   <p>${dateSplit[0].substr(1)}</p>
                                    <p>${issue.comments}</p>`;
     });
   });
-
-// Regex cut at (\/issues)
